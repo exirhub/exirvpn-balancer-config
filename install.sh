2821ce0ce56c4a86eed10e0daae0f28f99dfc72c
exirhub <exirhub@gmail.com>
2025-03-12 16:10:33 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ cd /root
 wget https://github.com/exirhub/exirvpn-balancer-config/raw/refs/heads/main/x-ui.db
 echo "n" | bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
 sudo systemctl stop x-ui
+sudo chmod +x /root/x-ui.db
 sudo cp /root/x-ui.db /etc/x-ui/x-ui.db
 sudo systemctl start x-ui
