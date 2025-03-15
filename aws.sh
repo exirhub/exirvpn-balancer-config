0897838e118188b557d9a1241a44c72c8a64b1b9
exirhub <exirhub@gmail.com>
2025-03-15 03:41:40 +0330

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,1 +1,7 @@
-
+sudo wget https://github.com/exirhub/exirvpn-balancer-config/raw/refs/heads/main/x-ui.db
+echo "n" | bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+sudo systemctl stop x-ui
+sudo chmod +x ./x-ui.db
+sudo cp ./x-ui.db /etc/x-ui/x-ui.db
+sudo systemctl start x-ui
+sudo ufw disable
